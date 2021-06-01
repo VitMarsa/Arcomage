41d76bbdb3d024a2f457ffd389287cb39ffbd85c
Vít Marša <marsavit@seznam.cz>
2021-06-01 18:02:09 +0200

    Verze 1.0
    Verze 1.0 Arcomage z lokálního uložiště.

---
+++ Arcomage/BackgroundWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Arcomage
+{
+    /// <summary>
+    /// Interakční logika pro BackgroundWindow.xaml
+    /// </summary>
+    public partial class BackgroundWindow : Window
+    {
+        private SpravceHry spravce;
+        private HerniObrazovka herniObrazovka;
+        private Pozadi pozadi;
+        public BackgroundWindow(HerniObrazovka herniObrazovka, SpravceHry spravce)
+        {
+            InitializeComponent();
+            this.herniObrazovka = herniObrazovka;
+            this.spravce = spravce;            
+            listBox_Backgrounds.ItemsSource = spravce.backgroundList.pozadiList;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox_Backgrounds.SelectedItem != null)
+            {
+                pozadi = (Pozadi)listBox_Backgrounds.SelectedItem;
+                herniObrazovka.Backgrounds = pozadi.Picture;
+            }
+            Close();
+        }
+    }
+}
---
+++ Arcomage/Pozadi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcomage
+{
+    public class Pozadi
+    {
+        public string Jmeno { get; set; }
+        public string Picture { get; set; }
+
+        public Pozadi(string jmeno, string picture)
+        {
+            Jmeno = jmeno;
+            Picture = picture;
+        }
+        public override string ToString()
+        {
+            return Jmeno;
+        }
+    }
+}
